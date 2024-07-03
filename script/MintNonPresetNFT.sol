3280737d8df3bbe72f00175d17ed5d31a3448ab6
Dane Shea <dane.shea@Danes-MBP.lan>
2024-07-03 12:34:56 +1000

    README update

--- script/MintNonPresetNFT.sol
+++ script/MintNonPresetNFT.sol
@@ -8,7 +8,6 @@ contract NFTDeployScript is Script {
     function run() external {
         // Load the private key from the environment variable
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
-        address recipient = vm.envAddress("RECIPIENT_ADDRESS");
         address tokenAddress = vm.envAddress("TOKEN_ADDRESS");
 
         // Load the deployer's address
@@ -20,7 +19,19 @@ contract NFTDeployScript is Script {
         // Deploy the NFT contract
         NFT nft = NFT(tokenAddress);
 
-        nft.mintTo(recipient);
+        // Array of recipient addresses
+        address[] memory recipients = new address[](6);
+        recipients[0] = 0x069b4848F3f991CA92d3400D40CDDEFaE6a7E549;
+        recipients[1] = 0xDF6C7A1b48750F220CF0fAb60A51eCF50049CAe8;
+        recipients[2] = 0xE52B84CEa14EDC85CeFC735Ed995B2E021e3a206;
+        recipients[3] = 0x0b7786D201f0Aaf0642f02D1fb931907dF6c7e2d;
+        recipients[4] = 0x1eD81E094cC225efD6ad4c2e9955e282aD02D2Cf;
+        recipients[5] = 0xE98B61832248C698085ffBC4313deB465BE857E7;
+
+        // Mint NFTs to each recipient
+        for (uint256 i = 0; i < recipients.length; i++) {
+            nft.mintTo(recipients[i]);
+        }
 
         vm.stopBroadcast();
     }
